9748fe18560eaae781703a6fe38204e73ae818a3
JuanPombo <juan.pombo@correounivalle.edu.co>
2025-04-11 11:34:30 -0500

    I tried to fix the error of break moving

--- tryChess/blankpygame.py
+++ tryChess/blankpygame.py
@@ -17,7 +17,6 @@ queen = pygame.image.load("queen.png")
 king = pygame.transform.scale(king,(40,80))
 queen = pygame.transform.scale(queen,(40,80))
 
-dt = 0
 kingPos = pygame.Vector2(260,480)
 mousePos = pygame.Vector2(0,0)
 #           WTorre1,WCaballo1,WAlfil1,WRey,WReina,WAlfil2,WCaballo2,WTorre2
@@ -29,13 +28,14 @@ mouseBoo = [False,False,False,False,False,False,False,False,
             False,False,False,False,False,False,False,False,
             False,False,False,False,False,False,False,False,]
 #pygame.draw.rect(DISPLAYSURF,GREEN,(0,0,640,640))
+mouseTem = [False,False,False]
 flag = False
 
 while True:
     for event in pygame.event.get():#Exit event
         if event.type == pygame.MOUSEBUTTONDOWN:
             mousePos = pygame.Vector2(pygame.mouse.get_pos())
-            mouseCont = 0
+            # mouseCont = 0
         if event.type == QUIT:
             pygame.quit()
             sys.exit() 
@@ -56,33 +56,14 @@ while True:
 
     DISPLAYSURF.blit(king,kingPos)
     DISPLAYSURF.blit(queen,(340,560))
-    """
-    keys = pygame.key.get_pressed()
-    if ((0 < player_pos.x < 600) and (0 < player_pos.y < 561)):
-        if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_pos.x += 300 * dt
-    elif (player_pos.x < 0):
-        player_pos = pygame.Vector2(player_pos.x+0.1, player_pos.y)
-    elif (player_pos.x > 600):
-        player_pos = pygame.Vector2(player_pos.x-0.1, player_pos.y)
-    elif (player_pos.y < 0):
-        player_pos = pygame.Vector2(player_pos.x,player_pos.y+0.1)
-    elif (player_pos.y > 561):
-        player_pos = pygame.Vector2(player_pos.x,player_pos.y-0.1)
-   
+    #print(mouseTem)
     
-    """  
     
-    if ((kingPos.x < mousePos.x < (kingPos.x + 80)) and (kingPos.y < mousePos.y < (kingPos.y + 80))): #Check if the mouse click-on the king
+    if ((kingPos.x < mousePos.x < (kingPos.x + 80)) and (kingPos.y < mousePos.y < (kingPos.y + 80))): #Check if the mouse is click-on the king
+        #print(mouseBoo[3])
         mouseBoo[3] = True
 
-    if mouseBoo[3] == True:     #I need that the King does to the Rect movement and the diagonal inferior derecha
+    if mouseBoo[3] == True:    
         #print(mouseBoo)
 
         if (((-80+kingPos.x)<mousePos.x<(kingPos.x + 160)) and ((-80+kingPos.y)<mousePos.y<(kingPos.y + 160))):
@@ -91,6 +72,7 @@ while True:
                 kingPos = pygame.Vector2(kingPos.x + 80,kingPos.y-80)
                 #print(mousePos,"Flag1")
                 mouseBoo[3] = False
+                print(mouseBoo[3])
             elif ((mousePos.x > kingPos.x+80) and (mousePos.y > kingPos.y+80)):#diagonal inferior derecha
                 kingPos = pygame.Vector2(kingPos.x + 80,kingPos.y+80)
                 mouseBoo[3] = False
@@ -114,9 +96,9 @@ while True:
             elif ((mousePos.x > kingPos.x+80)):#Recto derecha
                 kingPos = pygame.Vector2(kingPos.x+80,kingPos.y)
                 mouseBoo[3] = False
-            
+        else:
+            mouseBoo[3] = False
                 
-    dt = clock.tick(60)/1000
   
     
     pygame.display.flip()       
